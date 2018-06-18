96426f11bdec09765c1747f5f8faa4b71f7df853
Matt Gartner <mattgartner@gmail.com>
2018-06-17 19:23:07 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const dotenv = require("dotenv");
+dotenv.load();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello world!').end();
+});
+
+app.get('/test', (req, res) => {
+  res.status(200).send('Hello world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
